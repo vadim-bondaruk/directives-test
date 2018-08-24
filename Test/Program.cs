9ebd7fe5ceecd894a313a887228114e216d4c496
DESKTOP-N9P8UP5\HPG5 <v.bondarchuk@softteco.com>
2018-08-24 14:07:52 +0300

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test
+{
+    class ProgramB
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            Console.WriteLine("RELEASE");
+
+#elif DEBUG
+            Console.WriteLine("DEBUG");
+#endif
+            Console.ReadKey();
+        }
+    }
+}
